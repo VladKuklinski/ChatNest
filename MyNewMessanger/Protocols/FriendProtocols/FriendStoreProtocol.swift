c684db9006b85c3126b52a8bd3ec5efd54d9eb93
Vlad Kukliński <novikvladik0325@gmail.com>
2025-09-27 10:14:30 +0200

    Initial Commit

---
+++ MyNewMessanger/Protocols/FriendProtocols/FriendStoreProtocol.swift
@@ -0,0 +1,8 @@
+//
+//  FriendStoreProtocol.swift
+//  MyNewMessanger
+//
+//  Created by Vlad Kuklinski on 23/09/2025.
+//
+
+import Foundation
---
+++ MyNewMessanger/Protocols/UserServiceProtocol/UserServiceProtocol.swift
@@ -0,0 +1,21 @@
+//
+//  UserServiceProtocol.swift
+//  MyNewMessanger
+//
+//  Created by Vlad Kuklinski on 15/09/2025.
+//
+
+import Foundation
+import Combine
+
+protocol UserServiceProtocol {
+    
+    var currentUserPublisher: AnyPublisher<User?, Never> {get}
+    
+    var currentUser: User? { get set }
+    
+    func fetchUserData(uid : String?) async throws
+    
+    func fetchAllUsers() async throws -> [User]
+    
+}
